761824d5b0a4f8a558acd6ab76b51fc8fae68f69
sunokk125 <sunokk125@naver.com>
2021-08-14 15:25:30 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <button v-on:click="addTodo">add</button>
+    </div>
+</template>
+<script>
+export default {
+    data: function() {
+        return{
+            newTodoItem: ""
+        }
+    },
+    methods:{
+        addTodo: function(){
+            console.log(this.newTodoItem);
+            localStorage.setItem(this.newTodoItem,this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput: function(){
+            this.newTodoItem = "";
+        }
+    }
+}
+</script>
+<style scoped>
+input:focus{
+    outline: none;
+}
+.inputBox{
+    background: white;
+    
+}
+</style>
